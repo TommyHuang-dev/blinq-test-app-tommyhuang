3bf6535d1f947fe5f2681896c1794370677472eb
Yu-Tsai Fan <tracy.fan@blinqnetworks.com>
2023-04-25 12:47:22 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +21,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blinq-test-app';
+
+  months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+  dailyMeanTemp: ChartOptions = {
+    series: [
+      {
+        name: "Record high (°C)",
+        data: [16.1, 19.1, 26.7, 32.2, 34.4, 36.7, 40.6, 38.9, 37.8, 30.8, 23.9, 19.9]
+      },
+      {
+        name: "Daily mean (°C)",
+        data: [-3.7, -2.6, 1.4, 7.9, 14.1, 19.4, 22.3, 21.5, 17.2, 10.7, 4.9, -0.5]
+      },
+      {
+        name: "Record low (°C)",
+        data: [-32.8, -31.7, -26.7, -15.0, -3.9, -2.2, 3.9, 4.4, -2.2, -8.9, -20.6, -30.0]
+      }
+    ],
+    chart: {
+      height: 350,
+      type: "line"
+    },
+    title: {
+      text: "Temperature"
+    },
+    xaxis: {
+      categories: this.months
+    }
+  };
+
+  avgSnow: ChartOptions = {
+    series: [
+      {
+        name: "Average rainfall (mm)",
+        data: [29.1, 29.7, 33.6, 61.1, 82.0, 70.9, 63.9, 81.1, 84.7, 64.3, 75.4, 38.2]
+      },
+      {
+        name: "Average snowfall (cm)",
+        data: [37.2, 27.0, 19.8, 5.0, 0, 0, 0, 0, 0, 0.1, 8.3, 24.1]
+      }
+    ],
+    chart: {
+      height: 350,
+      type: "bar"
+    },
+    title: {
+      text: "Precipitation"
+    },
+    xaxis: {
+      categories: this.months
+    }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, NgApexchartsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
